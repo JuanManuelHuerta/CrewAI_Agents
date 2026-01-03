918d42654698c080499ce41057c080842a87d5cd
Juan Manuel Huerta <jmhuertany@gmail.com>
2026-01-03 10:30:44 -0500

    Improved the requirements and added an editor substep.

--- L6_Financial_Analyst.py
+++ L6_Financial_Analyst.py
@@ -22,25 +22,31 @@ scrape_tool = ScrapeWebsiteTool()
 
 portfolio_manager_agent = Agent(
     role="Portfolio Manager",
-    goal="Based on a basic theme, develop a main hypothesis (which is a mental model)"
-         " that brings together a set of financial factors and instruments. Also outline"
-         "a set of scenarios depending on how different factors play out.",
-    backstory="This is a portfolio manager in a multistrategy hedge fund "
+    goal="Based on a basic idea like {hypothesis} and those of {thought_leaders}, "
+        " manage and develop a concrete hypothesis which is falsifiable and objective "
+         " that brings together a set of financial factors like {financial_factors} "
+         " and trading instruments {instruments}. Also outline"
+         "a set of scenarios depending on how different factors play out."
+         "Ensure that the final output of the team is ready in the format required in {mental_framework}",
+    backstory="You are\ a portfolio manager in a multistrategy hedge fund "
               "specializing in capital and financial markets. This portfolio manager "
-              "uses macroeconomic ideas "
-              "to identify long term opportunities given current dynamics in the macroeconomic landscape. "
+              "uses macroeconomic ideas to create a mental model or hypothesis"
+                "to identify long term opportunities given current dynamics in the macroeconomic landscape. "
               "When possible, search and identify the key thought leaders and align with the ideas of those "
-              "that align well with the hypothesis.",
+              "that align well with the hypothesis. You have high mental discipline and insist on the highest standards."
+                "You admire Ray Dalio, and Bridgewater. You align with the mental framework required by them.",
     verbose=True,
-    allow_delegation=True,
+    allow_delegation=False,
     tools = [scrape_tool, search_tool]
 )
 strategist_analyst_agent = Agent(
     role="Strategist Analyst Agent",
-    goal="Flesh out the detailed trading strategies that will be implemented based on which of the  scenarios play out: when X happens then do Y.",
+    goal="Flesh out the detailed trading strategies that will be implemented based on how of the  scenarios  provided by the "
+        "Portfolio Manager play out: when X happens then do Y.",
     backstory="Equipped with a deep understanding of financial "
               "markets and quantitative analysis, this agent "
-              "devises and refines trading strategies based on the PM hypothesis and scenarios.",
+              "devises and refines trading strategies based on the PM hypothesis and scenarios."
+                " you also flesh out the causal linkages with more finesse and research them on the internet.",
     verbose=True,
     allow_delegation=False,
     tools = [scrape_tool, search_tool]
@@ -49,7 +55,7 @@ strategist_analyst_agent = Agent(
 trading_strategy_agent = Agent(
     role="Trading Strategy Agent",
     goal="Create very specific the tactical trading playbook to follow in the future"
-         "based on the hypothesis, strategies, and instruments and mechanisms available.",
+         "based on the hypothesis, strategies described in {tactical_strategies}, and instruments and mechanisms available.",
     backstory="This agent specializes in providing the playbook describing the timing, tactical steps, "
               "and logistical details of potential trades.",
     verbose=True,
@@ -57,6 +63,14 @@ trading_strategy_agent = Agent(
     tools = [scrape_tool, search_tool]
 )
 
+team_editor_agent = Agent(
+    role="Team Editor Agent",
+    goal="Responsible for ensuring the document follows the requirements outlines in the mental framework  {mental_framework}."
+         "Ensure that no content is redundant and that the whole document is self consistent.",
+    backstory="Professional editor and proofreader. You are aware of everybody's tasks and ensure that at the end of each review the document aligns with each steps requirements.",
+    verbose=True,
+    allow_delegation=False
+)
 risk_management_agent = Agent(
     role="Risk Advisor",
     goal="Evaluate and provide quantitative insights on the risks "
@@ -118,12 +132,24 @@ trading_tactics_development_task = Task(
     agent=trading_strategy_agent,
 )
 
+
+editor_task = Task(
+    description=(
+        "Editor that ensures quality, consistency, and homogenous document."
+    ),
+    expected_output=(
+        "4 page maximum length document aligning with the mental framework described in {mental_framework}. "
+    ),
+    agent=team_editor_agent,
+)
+
+
 # Task for Risk Advisor Agent: Assess Trading Risks
 risk_guardrails_task = Task(
     description=(
         "Given the hypothesis and scenario provided by the portfolio manager, and the"
         "trading strategy consider the risk of each scenario by calculating the value at"
-        "risk, and the likelihood of occurence and likelihood of outcome for each scenario."
+        "risk, and the likelihood of occurrence and likelihood of outcome for each scenario."
         "Use the risk analysis frameworks in {risk_analysis_frameworks}."
         "Perform drawdawn calculations."
     ),
@@ -135,6 +161,23 @@ risk_guardrails_task = Task(
     agent=risk_management_agent,
     output_file="L6_final.md"
 )
+
+wrap_up_task = Task(
+    description=(
+        "Given the work of the team, review and improve in a way that aligns with the framework in {mental_framework}."
+    ),
+    expected_output=(
+        "A comprehensive risk analysis report detailing potential "
+        "risks and mitigation recommendations for hypotheses and scenarios developed"
+        "in markdown format aligning with the framework in {mental_framework}."
+    ),
+    agent=portfolio_manager_agent,
+    output_file="L6_final.md"
+)
+
+
+
+
 from crewai import Crew, Process
 from langchain_openai import ChatOpenAI
 
@@ -144,12 +187,18 @@ financial_trading_crew = Crew(
     agents=[portfolio_manager_agent,
             strategist_analyst_agent,
             trading_strategy_agent,
-            risk_management_agent],
+            risk_management_agent,
+            team_editor_agent],
 
     tasks=[hypothesis_development_task,
+           editor_task,
            trading_strategy_development_task,
+            editor_task,
            trading_tactics_development_task,
-           risk_guardrails_task],
+            editor_task,
+           risk_guardrails_task,
+            editor_task,
+           wrap_up_task],
 
     manager_llm=ChatOpenAI(model="gpt-4o-mini",
                            temperature=0.7),
@@ -160,12 +209,14 @@ financial_trading_crew = Crew(
 # Example data for kicking off the process
 macro_perspective_inputs = {
     'instruments': 'Interest rates; currencies; derivatives' ,
-    'hypothesis': 'Modern Mercantilism: The effect of tariffs will introduce a new steady state in global finance. Artificial Intelligence will increase productivity."]',
+    'hypothesis': 'Modern Mercantilism: The effect of tariffs will introduce a new steady state in global finance. Artificial Intelligence will increase productivity. As described in https://www.bridgewater.com/research-and-insights/modern-mercantilism"]',
     'mechanisms': 'Long; Short; Carry Trades',
+    'mental_framework': 'https://www.bridgewater.com/forecasting-the-future-a-modern-economics-challenge',
+    'tactical_strategies': "https://www.investopedia.com/terms/t/tactical-trading.asp",
     'thought_leaders': 'Bridgewater Associates in https://www.bridgewater.com/the-growing-risk-to-us-assets-content-ctd',
-    'leverage': 'True',
-    'liquidity': 'Low',
-    'horizon': '24 months',
+    'leverage': 'Maximum leverage of 10 percent at any given time.',
+    'liquidity': 'No liquidity requirement at any given time.',
+    'horizon': 'investment horizon of 24 months',
     'risk_analysis_frameworks': 'https://www.investopedia.com/terms/v/var.asp',
     'financial_factors': 'Tariffs, Global Trade, Capital Investment Flows, Inflation, Interest Rates, Investment in AI',
     'news_impact_consideration': True
