022393c18f8842b505e85db9c0a5d13fb62e72ca
Juan Manuel Huerta <jmhuertany@gmail.com>
2026-01-07 20:31:20 -0500

    Masked Price Model

--- L6b_Shopper.py
+++ L6b_Shopper.py
@@ -55,14 +55,32 @@ data_collector = Agent(
     verbose=True
 )
 
+
+data_masker = Agent(
+    role='Data Masker',
+    goal='Perform masking operations on product data.',
+    backstory='Fidelity in passthrough data with randomized masking as described in task.',
+    verbose=True
+)
+
+data_estimator = Agent(
+    role='Data Estimator',
+    goal='Perform a quantitative and rational estimate of the masked value.',
+    backstory='Rational and Evidence based estimation.',
+    verbose=True
+)
+
+
+
 # Task for Strategist Analyst Agent: Analyze Market Data
 task1 = Task(
     description=(
         "Retrieve the detailed product specifications for query asin {target_asin} "
         " then search for a list of comparable products, and  using their ASIN strings save these and the target ASIN as a list of ASIN ids (products). "
         " Finally using  the key product specifications for the target and cohort asins  find the set of 5 to 10 attributes whose values are likely to determine product price for this cohort and target asin. "
+        " Please include brand, and material, if available."
     ),
-    expected_output="Find the list of 8 to 10 ASINs of comparable products (products) including the target ASIN and identify 5 to 10 product features that is common across the cohort (attributes).",
+    expected_output="Find the list of 8 to 10 ASINs of comparable products (products) including the target ASIN and identify 5 to 10 product features that is common across the cohort (attributes). Do not add any comments.",
     agent=shopper_agent,
     output_json=ProductAttributes,
     tools = [scrape_tool, search_tool],
@@ -85,15 +103,44 @@ task2 = Task(
     context=[task1]  # This gives task2 access to task1's output
 )
 
+task3 = Task(
+description="""
+     Receive the ProductDataList json object from the previous task and pick a product at random then mask the price of that 
+    product (i.e., replace the price with an -1.0 value (to keep it consistent with the type)). 
+         """,
+    agent=data_masker,
+    expected_output='Masked product data with prices and attribute values for all products. Do not add any comments.',
+    output_pydantic=ProductDataList,
+    context=[task2]  # This gives task2 access to task1's output
+
+
+
+)
+
+task4 = Task(
+description="""
+     Receive the Masked ProductDataList json object from the previous task and provide a rational estimate of the value that is masked.
+     Use only features, prices and attributes of the received masked object. Replace the product ID for this product with a 1 sentence rationale for the estimate.
+     Be as objective as possible and provide your thinking.  The output should be simply a JSON file following the provided pydantic object.
+         """,
+    agent=data_estimator,
+    expected_output='product data with prices and attribute values for all products. Replace the masked value (-1.0) with a rational estimate. Do not add any comments.',
+    output_pydantic=ProductDataList,
+    context=[task3]  # This gives task2 access to task1's output
+
+
+
+)
+
 from crewai import Crew, Process
 from langchain_openai import ChatOpenAI
 
 # Define the crew with agents and tasks
 shopper_crew = Crew(
 
-    agents=[shopper_agent, data_collector],
+    agents=[shopper_agent, data_collector, data_masker, data_estimator],
 
-    tasks=[task1, task2],
+    tasks=[task1, task2, task3,task4],
 
     manager_llm=ChatOpenAI(model="gpt-4o-mini",
                            temperature=0.7),
